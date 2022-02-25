260a425545ed43ea8e6c4162dbd7d628395a076f
Curtis Rueden <ctrueden@wisc.edu>
2022-02-25 10:42:22 -0600

    hats: add working formula computed recursively

--- hats.py
+++ hats.py
@@ -1,8 +1,10 @@
 import itertools
-print('hello')
+from math import factorial as fac
 
 # n! permutations of hats
 # but how many are "all wrong"?
+
+# What is the probability that the result will be all wrong?
 
 # if hat(i) == i, it's not "all wrong" -- throw these away (this hat(i) == i property is sometimes called "fixed")
 
@@ -14,6 +16,20 @@ print('hello')
 # -- n! - (n-1)! + (n-2)! - (n-3)! + ... ???
 # let's try it!
 
+def formula(n):
+    # GOAL:
+    # 1 = 0
+    # 2 = 1
+    # 3 = 2
+    # 4 = 9
+    # 5 = 44
+    # 6 = 265
+    # 7 = 1854
+    # 8 = 14833
+    # 9 = 133496
+    return formula(n-1) * n + (-1)**n if n > 1 else 0
+
+
 def count_all_wrong(n):
     count = 0
     for p in itertools.permutations(range(n)):
@@ -21,15 +37,21 @@ def count_all_wrong(n):
             if p[i] == i:
                 break
         else:
-            print(p)
+            #print(p)
             count += 1
     return count
 
 
 def main():
-    for n in range(1, 5):
-        result = count_all_wrong(n)
-        print(f'{n} = {result}')
+    for n in range(1, 10):
+        all_wrong = count_all_wrong(n)
+        f = formula(n)
+        assert all_wrong == f
+        prob = all_wrong / fac(n)
+        print(f'{n} = {all_wrong} (formula says {f}) -- probability is {prob}')
 
 if __name__ == '__main__':
     main()
+
+# "all wrong" is commonly known as a derangement. Or a subfactorial. Typically written !n instead of n!.
+# !N = N! * [1 − 1/!1 + 1/2! - 1/3! + 1/4! - ... + 1/N!]
